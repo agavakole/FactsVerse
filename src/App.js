1c250a1adcac75e73ccd0a62404a71e81da211e0
agavakole <agavakole@gmail.com>
2024-02-28 09:30:41 +0100

    Builing the app with react

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import './style.css';
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="64x64.png" alt="logo of the fact-verse" />
-        <h1>Facts Verse</h1>
-      </div>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="64x64.png" alt="logo of the fact-verse" />
+          <h1>Facts Verse</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a code fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a code fact</button>
+      </header>
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Fact List</section>;
 }
 
 export default App;
