d23a3e61751152c2908c4f1545ea6bd7aed747fa
agavakole <agavakole@gmail.com>
2024-02-28 08:40:20 +0100

    react files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import './src/style.css';
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="64x64.png" alt="logo of the fact-verse" />
+        <h1>Facts Verse</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a code fact</button>
+    </header>
+  );
 }
 
 export default App;
